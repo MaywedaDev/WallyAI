23dee11b5ae0976a609be10a181a20e095e53233
Enyo Onuche <enyoonuche1@gmail.com>
2025-04-10 23:13:49 +0100

    Initialised RN project

---
+++ src/app/(auth)/_layout.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Authlayout() {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  )
+}
