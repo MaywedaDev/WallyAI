a3a904060c0a1f90f7d64e92a1d7e218ba0776ea
Enyo Onuche <enyoonuche1@gmail.com>
2025-04-15 12:00:57 +0100

    Updated Code

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -6,24 +6,27 @@ import SearchInput from "@/components/SearchInput";
 import Trending from "@/components/Trending";
 import EmptyState from "@/components/EmptyState";
 import useAppwrite from "lib/useAppwrite";
-import { getAllPosts } from "lib/appwrite";
+import { getAllPosts, getLatestPosts } from "lib/appwrite";
 import VideoCard from "@/components/VideoCard";
+import VideoScreen from "@/components/VideoExample";
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = React.useState<string>("");
   const [refreshing, setRefreshing] = React.useState(false);
 
-  const { data, loading, error, refresh } = useAppwrite(getAllPosts);
+  const { data: posts, loading, error, refresh } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
-  const onRefresh = () => {
+  const onRefresh = async () => {
     setRefreshing(true);
+    await refresh();
     setRefreshing(false);
   };
   return (
     <SafeAreaView className="bg-primary flex-1">
       <FlatList
         // data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
-        data={data}
+        data={posts}
         keyExtractor={(item) => item.$id.toString()}
         renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
@@ -57,7 +60,9 @@ export default function Home() {
               </Text>
             </View>
 
-            <Trending posts={[{ $id: 1 }, { $id: 2 }, { $id: 3 }]} />
+            <Trending posts={latestPosts || []} />
+            {/* <VideoScreen />
+            <VideoScreen /> */}
           </View>
         )}
         ListEmptyComponent={
--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -4,16 +4,20 @@ import {
   FlatList,
   TouchableOpacity,
   ImageBackground,
+  Image,
 } from "react-native";
 import React from "react";
 import * as Animatable from "react-native-animatable";
+import { icons } from "@/constants";
+import { useVideoPlayer, VideoView } from "expo-video";
+import { useEvent } from "expo";
 
 const zoomIn = {
   0: {
     scale: 0.9,
   },
   1: {
-    scale: 1,
+    scale: 1.1,
   },
 };
 
@@ -27,26 +31,38 @@ const zoomOut = {
 };
 
 const TrendingItem = ({ activeItem, item }) => {
-  const [play, setPlay] = React.useState(false);
+  // const [play, setPlay] = React.useState(false);
+  const player = useVideoPlayer({ uri: item.video }, (player) =>
+    player.pause()
+  );
+  const { isPlaying } = useEvent(player, "playingChange", {
+    isPlaying: player.playing,
+  });
+
   return (
     <Animatable.View
       className="mr-5"
       animation={activeItem === item.$id ? zoomIn : zoomOut}
       duration={500}
     >
-      {play ? (
-        <Text className="text-white">Playing</Text>
+      {isPlaying ? (
+        <VideoView style={{ width: 208, height: 275 }} player={player} />
       ) : (
         <TouchableOpacity
           className="relative justify-center items-center"
           activeOpacity={0.7}
-          onPress={() => setPlay(true)}
+          onPress={() => player.play()}
         >
-          {/* <ImageBackground
+          <ImageBackground
             className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
             source={{ uri: item.thumbnail }}
             resizeMode="cover"
-          /> */}
+          />
+          <Image
+            className="w-12 h-12 absolute"
+            source={icons.play}
+            resizeMode="contain"
+          />
         </TouchableOpacity>
       )}
     </Animatable.View>
@@ -54,7 +70,14 @@ const TrendingItem = ({ activeItem, item }) => {
 };
 
 export default function Trending({ posts }) {
-  const [activeItem, setActiveItem] = React.useState(posts[0]);
+  const [activeItem, setActiveItem] = React.useState(posts[1]);
+
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
+
   return (
     <FlatList
       data={posts}
@@ -62,6 +85,18 @@ export default function Trending({ posts }) {
       renderItem={({ item }) => (
         <TrendingItem activeItem={activeItem} item={item} />
       )}
+      onViewableItemsChanged={({ viewableItems }) => {
+        if (viewableItems.length > 0) {
+          setActiveItem(viewableItems[0].key);
+        }
+      }}
+      viewabilityConfig={{
+        itemVisiblePercentThreshold: 70,
+      }}
+      contentOffset={{
+        x: 170,
+        y: 0,
+      }}
       horizontal
     />
   );
