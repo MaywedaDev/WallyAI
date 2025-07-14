9f1680ff04ba81b5760def4e1a44b86e78c2680d
Enyo Onuche <enyoonuche1@gmail.com>
2025-07-14 23:23:40 +0100

    Updated Code

--- src/app/(tabs)/bookmarks.tsx
+++ src/app/(tabs)/bookmarks.tsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useVideoPlayer, VideoView } from "expo-video";
+import FormField from "@/components/FormField";
+import { icons, images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import * as DocumentPicker from "expo-document-picker";
+import { router } from "expo-router";
+import { createVideo } from "lib/appwrite";
+import { useGlobalContext } from "context/GlobalProvider";
 
 export default function Bookmarks() {
   return (
-    <View>
-      <Text>Bookmarks</Text>
-    </View>
-  )
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="px-4 my-6 ">
+        <Text className="text-2xl text-white font-psemibold">Bookmarks</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
