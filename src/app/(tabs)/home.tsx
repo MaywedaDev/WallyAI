05e825f99c25ca6cf55490f325770f79579e343c
Enyo Onuche <enyoonuche1@gmail.com>
2025-07-17 23:12:19 +0100

    Updated Code

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -21,52 +21,5 @@ export default function Home() {
     await refresh();
     setRefreshing(false);
   };
-  return (
-    <SafeAreaView className="bg-primary flex-1">
-      <FlatList
-        // data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
-        data={posts}
-        keyExtractor={(item) => item.$id.toString()}
-        renderItem={({ item }) => <VideoCard video={item} />}
-        ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6">
-              <View>
-                <Text className="text-2xl text-gray-100 font-psemibold">
-                  WallyAI
-                </Text>
-              </View>
-              <View className="mt-1.5">
-                <Image
-                  source={images.logoSmall}
-                  className="w-9 h-10"
-                  resizeMode="contain"
-                />
-              </View>
-            </View>
-            <SearchInput placeholder="Search for a video topic" />
-
-            <View className="w-full flex-1 mt-12 pb-8">
-              <Text className="text-gray-100 font-pregular text-lg mb-3">
-                Latest Videos
-              </Text>
-            </View>
-
-            <Trending posts={latestPosts || []} />
-            {/* <VideoScreen />
-            <VideoScreen /> */}
-          </View>
-        )}
-        ListEmptyComponent={
-          <EmptyState
-            title="No Videos Found"
-            subtitle="Be the first one to upload a video"
-          />
-        }
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      />
-    </SafeAreaView>
-  );
+  return <SafeAreaView className="bg-primary flex-1"></SafeAreaView>;
 }
